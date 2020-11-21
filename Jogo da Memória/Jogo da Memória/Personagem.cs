c436fd2c9dcf0a9bdfac074a93c01c1fa96f5a1f
Luan Felipe <luanfelip952@gmail.com>
2020-11-21 12:52:41 -0300

    Classe Personagem

---
+++ Jogo da Memória/Jogo da Memória/Personagem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jogo_da_Memória
+{
+    class Personagem
+    {
+        string nome;
+        string fala;
+
+        public string Nome { get => nome; set => nome = value; }
+        public string Fala { get => fala; set => fala = value; }
+    }
+}
--- Jogo da Memória/Jogo da Memória/frmEscolhaPersonagem.Designer.cs
+++ Jogo da Memória/Jogo da Memória/frmEscolhaPersonagem.Designer.cs
@@ -42,11 +42,12 @@ namespace Jogo_da_Memória
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Font = new System.Drawing.Font("Milky Nice Clean", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(758, 734);
+            this.label2.Location = new System.Drawing.Point(505, 477);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(111, 39);
+            this.label2.Size = new System.Drawing.Size(76, 26);
             this.label2.TabIndex = 1;
             this.label2.Text = "label1";
             // 
@@ -54,11 +55,12 @@ namespace Jogo_da_Memória
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Milky Nice Clean", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(233, 734);
+            this.label1.Location = new System.Drawing.Point(155, 477);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(111, 39);
+            this.label1.Size = new System.Drawing.Size(76, 26);
             this.label1.TabIndex = 1;
             this.label1.Text = "label1";
             // 
@@ -67,9 +69,10 @@ namespace Jogo_da_Memória
             this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox2.BackgroundImage = global::Jogo_da_Memória.Properties.Resources.pers2;
             this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox2.Location = new System.Drawing.Point(648, 292);
+            this.pictureBox2.Location = new System.Drawing.Point(432, 190);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(315, 428);
+            this.pictureBox2.Size = new System.Drawing.Size(210, 278);
             this.pictureBox2.TabIndex = 0;
             this.pictureBox2.TabStop = false;
             // 
@@ -78,9 +81,10 @@ namespace Jogo_da_Memória
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.BackgroundImage = global::Jogo_da_Memória.Properties.Resources.pers1;
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox1.Location = new System.Drawing.Point(129, 292);
+            this.pictureBox1.Location = new System.Drawing.Point(86, 190);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(315, 428);
+            this.pictureBox1.Size = new System.Drawing.Size(210, 278);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -88,26 +92,28 @@ namespace Jogo_da_Memória
             // 
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Transparent;
-            this.label3.Font = new System.Drawing.Font("Milky Nice Clean", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(373, 165);
+            this.label3.Location = new System.Drawing.Point(249, 107);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(327, 39);
+            this.label3.Size = new System.Drawing.Size(214, 26);
             this.label3.TabIndex = 1;
             this.label3.Text = "Escolha seu amigo";
             // 
             // frmEscolhaPersonagem
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Jogo_da_Memória.Properties.Resources.back2;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1149, 853);
+            this.ClientSize = new System.Drawing.Size(766, 487);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.pictureBox2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmEscolhaPersonagem";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
