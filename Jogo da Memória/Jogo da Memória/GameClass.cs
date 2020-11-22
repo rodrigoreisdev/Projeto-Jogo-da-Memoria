55f4f34fc9fff058db04722be78af17fc4a29b25
RodrigoReis <rodrigoantonioreis@hotmail.com>
2020-11-22 16:03:20 -0300

    1

---
+++ Jogo da Memória/Jogo da Memória/GameClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jogo_da_Memória
+{
+    public class GameClass
+    {
+        string nivel;
+
+        public string Nivel { get => nivel; set => nivel = value; }
+    }
+}
